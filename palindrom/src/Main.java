dcce09b2823cd4a4d8e8400bf94392b42eaa0b96
osmanpampal28 <55186715+osmanpampal28@users.noreply.github.com>
2021-06-05 17:39:55 +0300

    palindrom

---
+++ palindrom/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(isPalindrome("abba"));
+    }
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+}
